cb83f388757e3cc604354862eba00e1a64e84461
Mbalenhle Bhengu <mbalenhlebhengu8@gmail.com>
2026-01-29 16:20:18 +0200

    added functionality for the select

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
-function updateTime() { 
-
-let joburgElement = document.querySelector("#joburg");
-let joburgDateElement = joburgElement.querySelector(".date");
-let joburgTimeElement = joburgElement.querySelector(".time");
-let joburgTime = moment().tz("Africa/Johannesburg");
-joburgDateElement.innerHTML = joburgTime.format("MMMM Do YYYY");
-joburgTimeElement.innerHTML = joburgTime.format("h:mm:ss [<small>]A[</small>]");
-
-
-let hiroshimaElement = document.querySelector("#hiroshima");
-let hiroshimaDateElement = hiroshimaElement.querySelector(".date");
-let hiroshimaTimeElement = hiroshimaElement.querySelector(".time");
-let hiroshimaTime = moment().tz("Asia/Tokyo");
-hiroshimaDateElement.innerHTML = hiroshimaTime.format("MMMM Do YYYY");
-hiroshimaTimeElement.innerHTML = hiroshimaTime.format(
-  "h:mm:ss [<small>]A[</small>]");
+function updateTime() {
+    
+  let joburgElement = document.querySelector("#joburg");
+  if (joburgElement) {
+  let joburgDateElement = joburgElement.querySelector(".date");
+  let joburgTimeElement = joburgElement.querySelector(".time");
+  let joburgTime = moment().tz("Africa/Johannesburg");
+  joburgDateElement.innerHTML = joburgTime.format("MMMM Do YYYY");
+  joburgTimeElement.innerHTML = joburgTime.format(
+    "h:mm:ss [<small>]A[</small>]",
+  );
+  }
+  
+  let hiroshimaElement = document.querySelector("#hiroshima");
+  if (hiroshimaElement) {
+  let hiroshimaDateElement = hiroshimaElement.querySelector(".date");
+  let hiroshimaTimeElement = hiroshimaElement.querySelector(".time");
+  let hiroshimaTime = moment().tz("Asia/Tokyo");
+  hiroshimaDateElement.innerHTML = hiroshimaTime.format("MMMM Do YYYY");
+  hiroshimaTimeElement.innerHTML = hiroshimaTime.format(
+    "h:mm:ss [<small>]A[</small>]",
+  );
 }
+} 
 
-updateTime()
+function updateCity(event) {
+  let cityTimezone = event.target.value;
+  let cityName = cityTimezone.split("/").pop().replace("_", " ");
+  let cityTime = moment().tz(cityTimezone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = ` <div class="city" id="joburg">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("h:mm:ss [<small>]A[</small>]")}</div>
+        </div>`;
+}
+updateTime();
 setInterval(updateTime, 1000);
+
+let citySelectElement = document.querySelector("#city");
+citySelectElement.addEventListener("change", updateCity);
